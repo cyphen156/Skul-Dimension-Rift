552b5c27b50ae771c100424f0bc82d055f81d2ba
cyphen156 <yungtaekoh@gmail.com>
2025-09-23 15:14:56 +0900

    [Init] Initialize Project
    1. Client
    2. DatabaseServer
    3. GameServer

---
+++ DatabaseServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
