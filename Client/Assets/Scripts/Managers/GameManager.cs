7329aa15cc127c709fc839007c59e8a2c990b273
cyphen156 <yungtaekoh@gmail.com>
2025-09-24 21:50:06 +0900

    [Add] Title Scene #1
    타이틀 씬 작업
    1. 레거시 애셋 이전 작업

---
+++ Client/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// 게임 매니저 싱글톤 클래스
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
