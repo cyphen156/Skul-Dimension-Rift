156b2fa6e1c774567895517505f14975c7f70134
cyphen156 <yungtaekoh@gmail.com>
2025-09-26 14:56:17 +0900

    [Feat] Title Secne #4 타이틀씬 진입 시퀀스 추가
    타이틀씬 진입 시퀀스 추가
    1. GameManager.cs
    MonoBehavior -> NetworkBehavior
    enum GameDifficulty 추가
    
    인터페이스
    IPlayable.cs 추가
    
    Intro.cs
    IPlayable 인터페이스 상속
    Public) Play()함수 추가
    GameManager의 GameDifficulty를 참조하여
    내부적으론 게임오브젝트를
    주기마다 변경하고, SoundManager에게 BGM 재생 요청

--- Client/Assets/Scripts/TitleScene/Intro.cs
+++ Client/Assets/Scripts/TitleScene/Intro.cs
@@ -1,16 +1,60 @@
+using Assets.Scripts.Interface;
 using UnityEngine;
+using static GameManager;
 
-public class TitleSequence : MonoBehaviour
+public class Intro : MonoBehaviour, IPlayable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // select background according to game Difficulty
+    [SerializeField]
+    private GameObject defaultBackground;
+    [SerializeField]
+    private GameObject hardModeBackground;
+    [SerializeField]
+    private string bgm;
+
+    // 난이도와 상관 없이 재생되는 배경
+    [SerializeField]
+    private GameObject neoWiz;
+    [SerializeField]
+    private GameObject southPaw;
+
+    #region Unity Methods
+
+    private void Awake()
     {
-        
+        defaultBackground = transform.Find("DefaultBackground").gameObject;
+        hardModeBackground = transform.Find("HardModeBackground").gameObject;
+        neoWiz = transform.Find("NeoWiz").gameObject;
+        southPaw = transform.Find("SouthPaw").gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        defaultBackground.SetActive(false);
+        hardModeBackground.SetActive(false);
+        neoWiz.SetActive(false);
+        southPaw.SetActive(false);
     }
+
+    #endregion Unity Methods
+
+    #region Custom Methods
+    public void Play()
+    {
+        GameDifficulty gm = GameManager.instance.GetGameMode();
+        if (gm == GameDifficulty.Hard)
+        {
+            hardModeBackground.SetActive(true);
+            defaultBackground.SetActive(false);
+            bgm = "HardModeIntroBGM";
+        }
+        else
+        {
+            defaultBackground.SetActive(true);
+            hardModeBackground.SetActive(false);
+            bgm = "DefaultIntroBGM";
+        }
+        SoundManager.instance.PlayBGM(bgm);
+    }
+    #endregion Custom Methods
 }
