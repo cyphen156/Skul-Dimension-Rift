09cc08e492ca32eecbd1370af6babb266a42db10
Cyphen <dbdxor@gmail.com>
2025-12-23 02:42:18 +0900

    [DeskTop -> Labtop] 개발환경 이전 푸시
    Nginx 준비

--- ApiServer/obj/Debug/net8.0/ApiServer.AssemblyInfo.cs
+++ ApiServer/obj/Debug/net8.0/ApiServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2843cde028429f8fff13bdc99d13b19d63961e89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91fa0d61399387e7ebc90062acc1ec6bbbd3577")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
