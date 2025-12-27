2d2bd584a529d668c42808194e7d27a1b26656f6
cyphen156 <yungtaekoh@gmail.com>
2025-12-27 17:21:38 +0900

    [Directory][Move] SubSystems 디렉토리 생성
    ScrollSystem 디렉토리 이전
    Stage -> SubSystem

---
+++ Client/Assets/Scripts/SubSystems/ContentSystem.cs
@@ -0,0 +1,16 @@
+using Assets.Scripts.Interface;
+
+public class ContentSystem : ISubSystem
+{
+    public void InitializeSubSystem()
+    {
+    }
+
+    public void TickSubSystem()
+    {
+    }
+
+    public void ShutdownSubSystem()
+    {
+    }
+}
