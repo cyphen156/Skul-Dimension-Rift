074043fb4bf746f64ec0139e569aecf476db655b
cyphen156 <yungtaekoh@gmail.com>
2026-01-14 21:40:57 +0900

    ㅁㄴㅇ러마ㅣㄴㅇ러
    끝내줘,,,,

---
+++ Client/Assets/Scripts/Interface/IStreamJob.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace Assets.Scripts.Interface
+{
+    public interface IStreamJob<T>
+    {
+        bool Execute(Stream stream, CancellationToken ct, out T result);
+    }
+}
---
+++ Client/Assets/Scripts/System/ContentManagementSystem.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// 컨텐츠 관련 정책을 관리하는 메인 시스템
+/// </summary>
+public class ContentManagementSystem
+{
+
+}
